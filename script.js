462f6b1ca795a12f2bccdb2f4d315ba11eb560b3
Ashish Kalbande <154236313+Ashishkalbande11@users.noreply.github.com>
2025-01-12 16:04:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+document.addEventListener('DOMContentLoaded', () => {
+	const container = document.querySelector('.container');
+
+	for(let i=0; i<800; i++){
+		const square = document.createElement('div');
+		square.className = 'square';
+		container.appendChild(square);
+
+
+		square.addEventListener('mouseover', () => {
+			const randomColor = `rgb(${Math.floor(Math.random()*256)},${Math.floor(Math.random()*256)},${Math.floor(Math.random()*256)})`
+			square.style.backgroundColor = randomColor
+			setTimeout(() => {
+			square.style.backgroundColor = 'rgb(29, 29, 29)'
+		},1000)
+		})
+
+		
+	}
+})
